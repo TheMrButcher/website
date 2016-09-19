6b083346320f86e119acf532b8306ce5f74899ac
TheMrButcher <slavnejshev@mail.ru>
2016-09-19 03:47:17 +0300

    Fixed setup

--- slavnejshev/config/deploy.rb
+++ slavnejshev/config/deploy.rb
@@ -14,28 +14,28 @@ namespace :foreman do
   desc 'Start server'
   task :start do
     on roles(:all) do
-      sudo "systemctl start #{application}"
+      sudo "systemctl start #{application}.target"
     end
   end
 
   desc 'Stop server'
   task :stop do
     on roles(:all) do
-      sudo "systemctl stop #{application}"
+      sudo "systemctl stop #{application}.target"
     end
   end
 
   desc 'Restart server'
   task :restart do
     on roles(:all) do
-      sudo "systemctl restart #{application}"
+      sudo "systemctl restart #{application}.target"
     end
   end
 
   desc 'Server status'
   task :status do
     on roles(:all) do
-      execute "systemctl | grep #{application}"
+      execute "systemctl | grep #{application}.target"
     end
   end
 end
@@ -56,26 +56,20 @@ namespace :deploy do
   desc 'Setup'
   task :setup do
     on roles(:all) do
-      execute "mkdir #{shared_path}/config/"
-      execute "mkdir /var/www/apps/#{application}/run/"
-      execute "mkdir /var/www/apps/#{application}/log/"
-      execute "mkdir /var/www/apps/#{application}/socket/"
-      execute "mkdir #{shared_path}/system"
+      execute "mkdir -p #{shared_path}/config/"
+      execute "mkdir -p /var/www/apps/#{application}/run/"
+      execute "mkdir -p /var/www/apps/#{application}/log/"
+      execute "mkdir -p /var/www/apps/#{application}/socket/"
+      execute "mkdir -p #{shared_path}/system"
 
       upload!('shared/database.yml', "#{shared_path}/config/database.yml")
       upload!('shared/Procfile', "#{shared_path}/Procfile")
       upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
       
       sudo 'systemctl stop nginx'
-      sudo "rm -f /usr/nginx/nginx.conf"
-      sudo "ln -s #{shared_path}/nginx.conf /usr/nginx/nginx.conf"
+      sudo "rm -f /etc/nginx/nginx.conf"
+      sudo "ln -s #{shared_path}/nginx.conf /etc/nginx/nginx.conf"
       sudo 'systemctl start nginx'
-
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "db:create"
-        end
-      end
     end
   end
 
@@ -85,6 +79,17 @@ namespace :deploy do
       execute "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
       execute "ln -s #{shared_path}/Procfile #{release_path}/Procfile"
       execute "ln -s #{shared_path}/system #{release_path}/public/system"
+    end
+  end
+  
+  desc 'Create DB'
+  task :create_db do
+    on roles(:all) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:create"
+        end
+      end
     end
   end
 
@@ -99,7 +104,7 @@ namespace :deploy do
         execute "cd #{current_path}"
         execute :bundle, "exec foreman export systemd #{foreman_temp} -a #{application} -u deploy -l /var/www/apps/#{application}/log -d #{current_path}"
       end
-      sudo "mv #{foreman_temp}/* /lib/systemd/system/"
+      sudo "rsync -a #{foreman_temp}/ /lib/systemd/system/"
       sudo "rm -r #{foreman_temp}"
     end
   end
@@ -116,6 +121,7 @@ namespace :deploy do
   before :setup, 'deploy:updating'
   before :setup, 'bundler:install'
   after :setup, 'deploy:symlink'
+  after :symlink, 'deploy:create_db'
   before :foreman_init, 'rvm:hook'
   after :setup, 'deploy:foreman_init'
   after :foreman_init, 'foreman:start'
